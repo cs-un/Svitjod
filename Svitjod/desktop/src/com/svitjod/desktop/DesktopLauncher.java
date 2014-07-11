2c27ae33acc1964025778c9028102963fc6f43e3
fyrvelm <fyrvelm@gmail.com>
2014-07-11 13:46:39 +0200

    1st

---
+++ Svitjod/desktop/src/com/svitjod/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.svitjod.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.svitjod.main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main(), config);
+	}
+}
