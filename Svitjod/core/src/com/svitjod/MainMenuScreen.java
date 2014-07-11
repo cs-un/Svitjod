c40b5653aedd7db98c9bd0fa4be15557d17bcd19
fyrvelm <fyrvelm@gmail.com>
2014-07-11 14:48:35 +0200

    b

--- Svitjod/core/src/com/svitjod/MainMenuScreen.java
+++ Svitjod/core/src/com/svitjod/MainMenuScreen.java
@@ -77,7 +77,7 @@ public class MainMenuScreen implements Screen {
 			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
 				if(isOver(button1, x, y)){	
 					buttonClicked = true;
-					//main.setScreen(new GameScreen(game, mms, false));
+					main.setScreen(new GameScreen(main, mms));
 				}
 			}
 		});
