599e7446a36d0899ed880a3a22073a68c36cf9e7
fyrvelm <fyrvelm@gmail.com>
2014-07-13 14:39:37 +0200

    f

--- Svitjod/core/src/com/entities/Building.java
+++ Svitjod/core/src/com/entities/Building.java
@@ -1,5 +1,21 @@
 package com.entities;
 
-public class Building {
+import java.util.ArrayList;
+
+public class Building extends Entity {
+
+	private int maxWorkers;
+	private ArrayList<Person> currentWorkers;
+	
+	public Building(int tile, int maxW) {
+		super(tile);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
