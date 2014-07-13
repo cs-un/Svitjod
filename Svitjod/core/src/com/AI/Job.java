a598ec3a7ecd9693d2c4651bc3aeb6eec4c31a08
fyrvelm <fyrvelm@gmail.com>
2014-07-13 14:48:39 +0200

    saker och ting

--- Svitjod/core/src/com/AI/Job.java
+++ Svitjod/core/src/com/AI/Job.java
@@ -1,5 +1,13 @@
 package com.AI;
 
+import com.entities.Building;
+
 public class Job {
 
+	private Building workplace;
+	
+	public Job(Building workplace)
+	{
+		this.workplace = workplace;
+	}
 }
--- Svitjod/core/src/com/entities/Building.java
+++ Svitjod/core/src/com/entities/Building.java
@@ -1,21 +1,45 @@
 package com.entities;
 
 import java.util.ArrayList;
+import java.util.Stack;
+
+import com.AI.Goal;
 
 public class Building extends Entity {
 
 	private int maxWorkers;
 	private ArrayList<Person> currentWorkers;
-	
+	public int offsetX, offsetY;
+	private Stack<Goal> work;
 	public Building(int tile, int maxW) {
 		super(tile);
+		currentWorkers = new ArrayList<Person>();
+		work = new Stack<Goal>();
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		if(!fullyEmployed())
+		{
+			/**
+			for(int i = 0; i < maxWorkers - currentWorkers.size;++i)
+			{
+				Map.createJobListing(this, i); // använd en hashmap i map sen så att inga duplicates skapas!
+			}
+			 */
+		}
+		
 		
 	}
+	
+	public boolean fullyEmployed()
+	{
+		return currentWorkers.size()==maxWorkers;
+	}
 
+	public Goal getWork() {
+		// TODO Auto-generated method stub
+		return work.pop();
+	}
 }
